ebe013bc57951d552f41da3719ee607c7ee5efc5
Zhastik <108812177+Zhastik@users.noreply.github.com>
2022-07-29 23:21:51 +0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,24 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
-from threading import Thread
 
 app = FastAPI()
-deb = FastAPI()
 
-@app.get("/")
-def healthcheck():
-    return 'Hello'
-
-@deb.get("/")
-def healthcheck():
-    return 'Привет'
-
-def func1():
-    uvicorn.run(app)
-
-def func2():
-    uvicorn.run(deb, port=1212)
-
-if __name__ == "__main__":
-    Thread(target = func1).start()
-    Thread(target = func2).start()
+@app.get(/)
+def home:
+	return ('Hello world')
