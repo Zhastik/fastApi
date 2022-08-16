999af134a2fc24980be162b9c9fe6a108fcbb508
Zhastik <108812177+Zhastik@users.noreply.github.com>
2022-08-17 02:32:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
-data = {"Name": None , "Language": None,}
+list_user_name = ['Artem', 'Andrey', 'Dima', 'Alex', 'Max']
+
+class Response(BaseModel):
+    new_W: str
 
 @app.get('/')
-def home():
-    return(data)
+async def home():
+    return(list_user_name)
 
-@app.post('/test')
-def test_post():
-    return(data)
+@app.post('/post/')
+async def test_post(response: Response):
+    new_data = response.text
+    list_user_name.append(new_data)
+    return {list_user_name}
+
+@app.put('/put/{user_name}')
+async def test_put(user_name: str):
+    print(user_name)
+    list_user_name.append(user_name)
+
+@app.delete("/delete/{user_name}")
+async def test_delete(user_name: str):
+    print(user_name)
+    list_user_name.remove(user_name)
+    return {
+        'deleted'
+    }
+
+@app.api_route("/pepls_list", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def handle_homedata(username: str):
+    print(username)
+    return {
+        'username':username
+    }
+
