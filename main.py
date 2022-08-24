287cc8f2f2ce258b939e2f42dc4c84dd0af3bd30
Zhastik <108812177+Zhastik@users.noreply.github.com>
2022-08-24 04:18:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,47 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-list_user_name = ['Artem', 'Andrey', 'Dima', 'Alex', 'Max']
+user_db = {
+    'Artem': {'name': 'Artem', 'date_birth': '24.03.2001', 'location': 'Samara'},
+    'Ivan': {'name': 'Ivan', 'date_birth': '12.08.1993', 'location': 'Moscow'},
+    'Dasha': {'name': 'Dasha', 'date_birth': '7.11.1998', 'location': 'Boston'}
+}
 
 class Response(BaseModel):
-    new_W: str
+    name: str
+    date_birth: str
+    location: str
 
-@app.get('/')
+@app.get('/user')
 async def home():
-    return(list_user_name)
+    list_user = list(user_db.values())
+    return(list_user)
 
-@app.post('/post/')
+@app.post('/user')
 async def test_post(response: Response):
-    new_data = response.text
-    list_user_name.append(new_data)
-    return {list_user_name}
+    new_name = response.name
+    user_db[new_name] = response.dict()
+    return {'200'}
 
-@app.put('/put/{user_name}')
-async def test_put(user_name: str):
-    print(user_name)
-    list_user_name.append(user_name)
+@app.put('/user')
+async def test_put(response: Response):
+    new_name = response.name
+    user_db[new_name] = response.dict()
+    return {'200'}
 
 @app.delete("/delete/{user_name}")
 async def test_delete(user_name: str):
-    print(user_name)
-    list_user_name.remove(user_name)
+    del user_db[user_name]
     return {
         'deleted'
     }
+
+@app.patch('/user')
+async def test_put(response: Response):
+    new_name = response.name
+    user_db[new_name].update(response.dict())
+    user_db[new_name] = response.dict(exclude_unset=True)
+    return {'200'}
 
 @app.api_route("/pepls_list", methods=['GET', 'POST', 'PUT', 'DELETE'])
 def handle_homedata(username: str):
